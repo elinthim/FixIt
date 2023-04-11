a2ae682b26c02250957d40826037bd7ac9ff1a00
elinthim <elin_thim@hotmail.com>
2023-04-11 22:46:06 +0200

    Add project files.

---
+++ FixIt/Controllers/ToDoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FixIt.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //hömta todo items
+            //skicka items till en modell
+            //skicka resultatet till en view
+            return View();
+        }
+    }
+}
---
+++ FixIt/Models/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace FixIt.Models
+{
+    public class ToDoItem
+    {
+        [Key]
+        public Guid ToDoItemId { get; set; } 
+        public Boolean IsDone { get; set; }
+        [Required]
+        [StringLength(maximumLength:50, MinimumLength =3)]
+        public string Title { get; set; } = string.Empty;
+        public DateTimeOffset? DueAt { get; set; }  
+    }
+}
